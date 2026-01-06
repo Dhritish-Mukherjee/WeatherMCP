cf950b07c5fa2f0cc0145cc542e7fb0398da2617
Dhritish Mukherjee <coderdhritish18@gmail.com>
2026-01-06 01:57:33 +0000

    checking for http req/res

---
+++ bindings.rs
@@ -0,0 +1,73 @@
+
+use serde::{Deserialize, Serialize};
+use weil_macros::{constructor, mutate, query, secured, smart_contract, WeilType};
+use weil_rs::collections::{streaming::ByteStream, plottable::Plottable};
+use weil_rs::config::Secrets;
+use weil_rs::webserver::WebServer;
+
+
+trait JokeTest {
+    fn new() -> Result<Self, String>
+    where
+        Self: Sized;
+    async fn get_joke(&self) -> Result<String, String>;
+    fn tools(&self) -> String;
+    fn prompts(&self) -> String;
+}
+
+#[derive(Serialize, Deserialize, WeilType)]
+pub struct JokeTestContractState {
+    // define your contract state here!
+}
+
+#[smart_contract]
+impl JokeTest for JokeTestContractState {
+     #[constructor]
+    fn new() -> Result<Self, String> {
+        Ok(Self {})
+    }
+
+
+    #[query]
+    async fn get_joke(&self) -> Result<String, String> {
+        let url = "https://official-joke-api.appspot.com/random_joke";
+        
+        let response = HttpClient::request(url, HttpMethod::Get)
+            .send()
+            .map_err(|e| format!("HTTP error: {}", e))?;
+        
+        if response.status() >= 200 && response.status() < 300 {
+            Ok(response.text())
+        } else {
+            Err(format!("Failed: HTTP {}", response.status()))
+        }
+    }
+
+
+    #[query]
+    fn tools(&self) -> String {
+        r#"[
+  {
+    "type": "function",
+    "function": {
+      "name": "get_joke",
+      "description": "",
+      "parameters": {
+        "type": "object",
+        "properties": {},
+        "required": []
+      }
+    }
+  }
+]"#.to_string()
+    }
+
+
+    #[query]
+    fn prompts(&self) -> String {
+        r#"{
+  "prompts": []
+}"#.to_string()
+    }
+}
+
